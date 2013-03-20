9374eb544bb0e5f451329385fa5923361574d6cd
rmckenzie <rmckenzie92@gmail.com>
2013-03-19 20:54:38 -0500

    Comment up sad/util
    This file is probably going to get moved to sad/runtime.clj and then
    further reworked to support the idea of a library for interacting with
    parsers generated by sad but for now comment it up some.

--- src/me/arrdem/sad/util.clj
+++ src/me/arrdem/sad/util.clj
@@ -1,21 +1,25 @@
-(ns me.arrdem.sad.util
+(ns ^{:doc     "Implements the runtime hooks which sad parsers can use."
+      :author  "Reid McKenzie"
+      :version "0.1.2"}
+  me.arrdem.sad.util
   (:require [clojure.pprint :refer :all]))
 
-(def nothing (fn [&  _] nil))
-(def code-pp #(with-pprint-dispatch code-dispatch (pprint %)))
+(def nothing
+  "A varargs function which returns nil doing nothing. Used as a placeholder
+for when a hook is called for and there is no registered side-effect hook."
+  (fn [&  _] nil))
 
-(def bnf-semantics-registry  (atom {}))
-(def bnf-hooks-registry      (atom {}))
+;; ToDo move code-pprint somewhere more general
+(def code-pp
+  "A wrapper around pprint which makes its code-formatting setting more
+easily accessed."
+  #(with-pprint-dispatch code-dispatch (pprint %)))
 
-(defn get-prefixed-hook [prefix sym]
-  (or ((symbol prefix (name sym))
-       @bnf-hooks-registry) nothing))
+;;------------------------------------------------------------------------------
+;;------------------------------------------------------------------------------
+;; Side-effect "hook" system
 
-(def get-post-hook (partial get-prefixed-hook "post"))
-(def get-pre-hook (partial get-prefixed-hook "pre"))
-
-(defn get-semantics [sym]
-  (or (sym @bnf-semantics-registry) identity))
+(def bnf-hooks-registry (atom {}))
 
 (defn set-prefixed-hook [prefix sym fn]
   (swap! bnf-hooks-registry
@@ -26,7 +30,37 @@
 (def set-post-hook (partial set-prefixed-hook "post"))
 (def set-pre-hook  (partial set-prefixed-hook "pre"))
 
-(defn set-semantics [sym fn]
+(defn get-prefixed-hook
+  "Searches the prefix table for a prefixed symbol hook, returning the hook
+if one is registered otherwise returning the function Nothing."
+  [prefix sym]
+  (or ((symbol prefix (name sym))
+       @bnf-hooks-registry) nothing))
+
+(def get-post-hook
+  "Wrapper on get-prefixed-hook which instructs it to look for a \"post\" hook."
+  (partial get-prefixed-hook "post"))
+
+(def get-pre-hook
+  "Wrapper on get-prefixed-hook which instructs it to look for a \"pre\" hook."
+  (partial get-prefixed-hook "pre"))
+
+;;------------------------------------------------------------------------------
+;;------------------------------------------------------------------------------
+;; Semantics manipulation system
+
+(def bnf-semantics-registry (atom {}))
+
+(defn get-semantics [sym]
+  "Searches the semantics table for a transform function, returning
+clojure.core/identity if there is no registered transform."
+  (or (sym @bnf-semantics-registry) identity))
+
+(defn set-semantics
+  "Relates the function or expression fn to the symbol sym in the semantics
+table, evaluating the expression with the expectation of receiving a function
+if the fn argument does not already implement clojure.lang.IFn."
+  [sym fn]
   (if (instance? clojure.lang.IFn fn)
     (swap! bnf-semantics-registry assoc sym fn)
     (swap! bnf-semantics-registry assoc sym (eval fn))))
