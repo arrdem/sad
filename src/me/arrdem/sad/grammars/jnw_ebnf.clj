5b01d7d85f3d82659bfdb4b98156dd1ee5659751
rmckenzie <rmckenzie92@gmail.com>
2013-03-19 20:52:02 -0500

    Break lexers/util out of grammars/util & rework
    Much of the code in grammars/util was attempting to clean up the
    implementation of the two grammars in me.arrdem.sad.grammars. This patch
    migrates much of that code out into its own file elsewhere on the
    classpath, and reworks both grammars to draw out common code generation
    operations into grammars/util.
    
    Also added namespace annotations and comments where potentilly valuable.

--- src/me/arrdem/sad/grammars/jnw_ebnf.clj
+++ src/me/arrdem/sad/grammars/jnw_ebnf.clj
@@ -1,77 +1,75 @@
-(ns me.arrdem.sad.grammars.jnw-ebnf
-  (:require [lexington.lexer       :refer :all]
+(ns ^{:doc    "An implementation of a lexer, parser and code gen for EBNF as
+               used in The Pascal User Manual and Report 4th ed. by Wirth and
+               Jensen. Hence the name jnw-ebnf."
+      :author "Reid McKenzie"
+      :added  "0.1.0"}
+  me.arrdem.sad.grammars.jnw-ebnf
+  (:require [lexington.lexer :refer :all]
             [lexington.utils.lexer :refer :all]
             [name.choi.joshua.fnparse :as fnp]
-            [me.arrdem.sad.grammars.util :as util]
-            [me.arrdem.sad.util :as runtime-util]))
+            [me.arrdem.sad.grammars.util :as gutil]
+            [me.arrdem.sad.lexers.util :as lutil :refer [make-lexer
+                                                           deftoken]]))
 
-(deflexer jnw-ebnf-base
-  :lbracket "["
-  :rbracket "]"
-  :lparen   "("
-  :rparen   ")"
-  :lbrace   "{"
-  :rbrace   "}"
-  :assign   "="
-  :or       "|"
-  :dot      "."
-  :string   util/good-string-re
-  :word     #"[a-zA-Z\-]+"
-  :ws       #" |\t|\r|\n"
-  :comment  #";+.*[\n\r]+"
-  :chr      #".")
+(lutil/make-lexer jnw-ebnf-base
+  :ws lutil/whitespace
+  :comment lutil/comment
+  (deftoken lbracket "[")
+  (deftoken rbracket "]")
+  (deftoken lparen "(")
+  (deftoken rparen ")")
+  (deftoken lbrace "{")
+  (deftoken rbrace "}")
+  (deftoken equals "=")
+  (deftoken ortok "|")
+  (deftoken dot ".")
+  (deftoken Terminal lutil/string)
+  (deftoken NonTerminal lutil/word)
+  :chr #"."
+  )
 
 (def jnw-ebnf-lexer
   (-> jnw-ebnf-base
       (discard :ws)
       (discard :comment)
-      (generate-for :word    :val util/reader)
-      (generate-for :string  :val util/reader)
-      (generate-for :chr     :val util/wordfn)))
+      (generate-for :word    :val lutil/readerfn)
+      (generate-for :string  :val lutil/readerfn)
+      (generate-for :chr     :val lutil/wordfn)))
 
-(util/deftoken NonTerminal :word)
-(util/deftoken Terminal    :string)
-(util/deftoken equals      :assign)
-(util/deftoken dot         :dot)
-(util/deftoken lparen      :lparen)
-(util/deftoken rparen      :rparen)
-(util/deftoken lbracket    :lbracket)
-(util/deftoken rbracket    :rbracket)
-(util/deftoken lbrace      :lbrace)
-(util/deftoken rbrace      :rbrace)
-(util/deftoken ortok       :or)
+;;------------------------------------------------------------------------------
+;; Declare & define productions
 
 (declare Syntax Production Expression Term Factor)
 
 (def Expression
   (fnp/semantics
-   (fnp/conc
-    Term
-    (fnp/rep*
-     (fnp/conc
-      ortok
-      Term)))
-   (fn [[t terms]]
-     (let [terms (map second terms)]
-       (if-not (empty? terms)
-         `(~'fnp/alt ~t ~@terms)
-         t)))))
+   (fnp/conc Term
+             (fnp/rep*
+              (fnp/conc ortok
+                        Term)))
+   gutil/expression-compiler))
 
 (def opt-expr
   (fnp/semantics
-   (fnp/conc lbracket Expression rbracket)
+   (fnp/conc lbracket
+             Expression
+             rbracket)
    (fn [[_0 expr _1]]
      `(~'fnp/opt ~expr))))
 
 (def rep*-expr
   (fnp/semantics
-   (fnp/conc lbrace Expression rbrace)
+   (fnp/conc lbrace
+             Expression
+             rbrace)
    (fn [[_0 expr _1]]
      `(~'fnp/rep* ~expr))))
 
 (def alt-expr
   (fnp/semantics
-   (fnp/conc lparen Expression rparen)
+   (fnp/conc lparen
+             Expression
+             rparen)
    (fn [[_0 expr _1]]
      `(~'fnp/alt ~expr))))
 
@@ -80,8 +78,7 @@
    NonTerminal
    (fnp/semantics
     Terminal
-    (fn [x]
-      `(~'fnp/lit ~x)))
+    gutil/literal-compiler)
    opt-expr
    rep*-expr
    alt-expr))
@@ -89,35 +86,25 @@
 (def Term
   (fnp/semantics
    (fnp/rep+ Factor)
-   (fn [factors]
-     (if (< 1 (count factors))
-       `(~'fnp/conc ~@factors)
-       (first factors)))))
+   gutil/term-compiler))
 
 (def Production
   (fnp/semantics
-   (fnp/conc
-    NonTerminal
-    equals
-    Expression
-    dot)
-   (fn [[sym _ exp]]
-     (util/register-sym sym)
-     `(def ~sym (~'fnp/conc
-                 (~'fnp/effects
-                  (~'util/get-pre-hook (quote ~sym)))
-                 (~'fnp/semantics ~exp
-                  (~'util/get-semantics (quote ~sym)))
-                 (~'fnp/effects
-                  (~'util/get-post-hook (quote ~sym))))))))
+   (fnp/conc NonTerminal
+             equals
+             Expression
+             dot)
+   gutil/production-compiler))
 
 (def Syntax
   (fnp/rep+ Production))
 
+;;------------------------------------------------------------------------------
+;; And throw a run interface on this grammar
+
 (defn run [{str ":str" srcfile ":srcfile"}]
-  (-> (if str
-        str
+  (-> (if str str
         (slurp srcfile))
       jnw-ebnf-lexer
-      (#(util/fnparse-run Syntax %1))
-      (#(concat (util/make-bnf-file-prefix) %1))))
+      (#(gutil/fnparse-run Syntax %1))
+      (#(concat (gutil/make-bnf-file-prefix) %1))))
--- src/me/arrdem/sad/grammars/util.clj
+++ src/me/arrdem/sad/grammars/util.clj
@@ -1,51 +1,88 @@
-(ns me.arrdem.sad.grammars.util
-  (:require [lexington.lexer       :refer :all]
-            [lexington.utils.lexer :refer :all]
-            [name.choi.joshua.fnparse :as fnp]
+(ns ^{:doc    "A collection of utilities for the various grammars. Really a
+               holding pen for code which will ultimately be better managed."
+      :author "Reid McKenzie"}
+  me.arrdem.sad.grammars.util
+  (:require [name.choi.joshua.fnparse :as fnp]
             [clojure.set           :refer [union]]))
 
-(defmacro deftoken [symbol val]
-  `(def ~symbol
-     (fnp/semantics
-      (fnp/term
-       #(= (:lexington.tokens/type %1) ~val))
-      :val)))
-
-(def whitespace-re    #" |\t|\r|\n")
-(def simple-string-re #"\"[^\"]+\"")
-(def good-string-re   #"\"[^\"\\]*(?:\\.[^\"\\]*)*\"")
-
-(defn fnparse-run [rule tokens]
-  (apply vector
-         (fnp/rule-match
-          rule
+(defn fnparse-run
+  "A wrapper to shorten fnparse's unfortunately long invocation"
+  [rule tokens]
+  (fnp/rule-match rule
           #(println "FAILED: " %)
           #(println "LEFTOVER: " %2)
-          {:remainder tokens})))
+          {:remainder tokens}))
 
-(def reader
-  (comp read-string
-        #(apply str %)
-        :lexington.tokens/data))
+;;------------------------------------------------------------------------------
+;; emitted code prefix generation
 
-(def wordfn
-  (fn [v]
-    (apply str
-           (:lexington.tokens/data v))))
+(def ^{:public false :static false} sym-registry
+  "The set of symbols which the grammar being parsed contains. Used to generate
+forward-declarations at code emission time in order to resolve any circular rule
+dependence issues and as opposed to doing a lot of work to resolve the dependance
+order of the rules at hand."
+  (atom #{}))
 
-(def strfn
-  (fn [v]
-    (-> (:lexington.tokens/data v)
-        butlast
-        (#(drop 1 %1))
-        (#(apply str %1)))))
-
-(def sym-registry (atom #{}))
-
-(defn register-sym [sym]
+(defn register-sym
+  "Adds a symbol to the symbol registry"
+  [sym]
   (swap! sym-registry union #{sym}))
 
-(defn make-bnf-file-prefix []
+(defn clear-symbols
+  "Clears the symbol registry"
+  []
+  (reset! sym-registry #{}))
+
+(defn make-bnf-file-prefix
+  "Generates the (require) statements necessary to make the symbols which the
+code emitter uses available to the namespace in which the emitted code is
+evaluated. Also generates the forward declarations from the set of registered
+symbols."
+  []
   [`(require ['name.choi.joshua.fnparse :as '~'fnp]
              ['me.arrdem.sad.util :as '~'util])
    `(declare ~@(deref sym-registry))])
+
+;;------------------------------------------------------------------------------
+;; FNParse code emitters
+
+(defn expression-compiler
+  "A code gen for the 'expression' rule form in BNF, being a sequence of legal
+alternatives. Compiles to an fnp/alt call in the multi case, otherwise skips
+the superfluous (alt) call in favor of the literal first argument."
+  [[t terms]]
+  (let [terms (map second terms)]
+    (if-not (empty? terms)
+      `(~'fnp/alt ~t ~@terms)
+      t)))
+
+(defn literal-compiler
+  "A code gen for literals"
+  [x]
+  `(~'fnp/lit ~x))
+
+(defn term-compiler
+  "A code gen for a sequence of sequentially requisite rules. Builds to a (conc)
+form in the case of multiple arguments, otherwise gives the first of its
+arguments, assuming that the usage was in the context of a one or more match."
+  [factors]
+  (if (< 1 (count factors))
+    `(~'fnp/conc ~@factors)
+    (first factors)))
+
+(defn production-compiler
+  "A code generation for the basic symbol definition case. Registers the symbol
+with the symbol set atom so that it will be listed in the generated forward
+declaration. Generates the (def) form, and provides a wrapper around the
+argument rule which attaches sad's runtime hooks."
+  [[sym _ exp]]
+  (register-sym sym)
+  `(def ~sym (~'fnp/semantics
+              (~'fnp/conc
+               (~'fnp/effects
+                ((~'util/get-pre-hook (quote ~sym))))
+               (~'fnp/semantics ~exp
+                                (~'util/get-semantics (quote ~sym)))
+               (~'fnp/effects
+                ((~'util/get-post-hook (quote ~sym)))))
+              second)))
