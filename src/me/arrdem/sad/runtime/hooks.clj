fd747da169f4eb6ac85ea4ac97f04ae4e12403ce
rmckenzie <rmckenzie92@gmail.com>
2013-03-26 17:39:26 -0500

    patch so that the hook fns won't explode for unset ns vars

--- src/me/arrdem/sad/runtime/hooks.clj
+++ src/me/arrdem/sad/runtime/hooks.clj
@@ -8,9 +8,13 @@
 (defn get-pre-hook
   "Wrapper on get-prefixed-hook which instructs it to look for a \"pre\" hook."
   [sym]
-  (or (ns-resolve *pre-hook-ns* sym) nothing))
+  (if (and *pre-hook-ns* (symbol? *pre-hook-ns*))
+    (ns-resolve *pre-hook-ns* sym)
+    nothing))
 
 (defn get-post-hook
   "Wrapper on get-prefixed-hook which instructs it to look for a \"post\" hook."
   [sym]
-  (or (ns-resolve *post-hook-ns* sym) nothing))
+  (if (and *post-hook-ns* (symbol? *post-hook-ns*))
+    (ns-resolve *post-hook-ns* sym)
+    nothing))
